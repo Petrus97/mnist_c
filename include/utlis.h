1d269231244d0cc25692b13970cfd7a50aa54780
ale19 <ale.petru97@gmail.com>
2023-12-27 01:27:03 +0100

    added some defines

--- include/utlis.h
+++ include/utlis.h
@@ -3,6 +3,16 @@
 
 #define BUFFER_SIZE(buffer) sizeof(buffer) / sizeof(buffer[0])
 
+#define NUM_ROWS(m) \
+    BUFFER_SIZE(m)
+
+#define NUM_COLS(m) \
+    BUFFER_SIZE(m[0])
+
+#define MATRIX_SIZE(m) \
+    NUM_ROWS(m) * NUM_COLS(m)
+
+
 // https://stackoverflow.com/questions/2182002/how-to-convert-big-endian-to-little-endian-in-c-without-using-library-functions
 #define swap_endianess_32(num)                                                                                         \
     ((num >> 24) & 0xff) | ((num << 8) & 0xff0000) | ((num >> 8) & 0xff00) | ((num << 24) & 0xff000000)
